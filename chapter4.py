2cb83b3f064a1c7686ba8809525ffa5e8651299a
joao.fernandes <joao.fernandes@blip.pt>
2018-01-23 19:09:44 +0000

    SGD regression with sklearn

--- chapter4.py
+++ chapter4.py
@@ -1,14 +1,19 @@
 # %%
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import SGDRegressor
 
+# %%
+# Linear regression using normal equation
 X = 2 * np.random.rand(100, 1)
-y = 4 + 3 * X + np.random.rand(100, 1)
+y = 4 + 3 * X + np.random.randn(100, 1)
 
 X_b = np.c_[np.ones((100,1)), X] # add bias parameter
 theta_best = np.linalg.inv(X_b.T.dot(X_b)).dot(X_b.T).dot(y)
 
 # %%
+# Predict on test set and plot everything
 X_new = np.array([[0], [2]])
 X_new_b =  np.c_[np.ones((2, 1)), X_new]
 y_predict = X_new_b.dot(theta_best)
@@ -17,3 +22,54 @@ plt.plot(X_new, y_predict, "r-")
 plt.plot(X, y, "b.")
 plt.axis([0, 2, 0, 15])
 plt.show()
+
+# %%
+# Linear regrassion using sklearn
+lin_reg = LinearRegression()
+lin_reg.fit(X, y)
+lin_reg.intercept_, lin_reg.coef_
+lin_reg.predict(X_new)
+
+# %%
+# Batch gradient descent
+eta = 0.1 # learning rate
+n_iterations = 1000
+m = 100
+
+theta = np.random.randn(2, 1) # random initialization
+
+for iteration in range(n_iterations):
+  gradients  = 2/m * X_b.T.dot(X_b.dot(theta) - y)
+  theta = theta - eta * gradients
+
+theta
+
+# %%
+# Stochastic gradient descent with simple learning schedule
+n_epochs = 50
+t0, t1 = 5, 50
+
+def learning_schedule(t):
+  return t0 / (t + t1)
+
+theta = np.random.randn(2, 1) # random init
+
+for epoch in range(n_epochs):
+  for i in range(m):
+    random_index = np.random.randint(m)
+    xi = X_b[random_index:random_index+1]
+    yi = y[random_index:random_index+1]
+    gradients = 2 * xi.T.dot(xi.dot(theta) - yi)
+    eta = learning_schedule(epoch * m + i)
+    theta = theta - eta * gradients
+  
+theta
+
+# %%
+# Linear regression using stochastic gradient descent with sklearn
+sgd_reg = SGDRegressor(n_iter=50, penalty=None, eta0=0.1)
+sgd_reg.fit(X, y.ravel()) # ravel is used to flatten y into a 1d array
+
+sgd_reg.intercept_, sgd_reg.coef_
+
+    
