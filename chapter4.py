b11b27562faaaa0d752600415b54a98f78576c76
joao.fernandes <joao.fernandes@blip.pt>
2018-01-29 14:37:20 +0000

    Continue chapter 4

--- chapter4.py
+++ chapter4.py
@@ -3,6 +3,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import SGDRegressor
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split]
+from sklearn.pipeline import Pipeline
 
 # %%
 # Linear regression using normal equation
@@ -72,4 +76,40 @@ sgd_reg.fit(X, y.ravel()) # ravel is used to flatten y into a 1d array
 
 sgd_reg.intercept_, sgd_reg.coef_
 
-    
+#%% 
+# Polinomial regression
+m = 100
+X = 6 * np.random.rand(m, 1) - 3
+y = 0.5 * X**2 + X + 2 + np.random.rand(m, 1) 
+
+poly_features = PolynomialFeatures(degree=2, include_bias=False) # generate polinomial features -> add the square of each feature as a new feature
+X_poly = poly_features.fit_transform(X)
+
+lin_reg = LinearRegression()
+lin_reg.fit(X_poly, y)
+lin_reg.intercept_, lin_reg.coef_
+
+# %% 
+# Plot learning curves
+def plot_learning_curves(model, X, y):
+  X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2)
+  train_errors, val_errors = [], []
+  for m in range (1, len(X_train)):
+    model.fit(X_train[:m], y_train[:m])
+    y_train_predict = model.predict(X_train[:m])
+    y_val_predict = model.predict(X_val)
+    train_errors.append(mean_squared_error(y_train_predict, y_train[:m]))
+    val_errors.append(mean_squared_error(y_val_predict, y_val))
+  plt.plot(np.sqrt(train_errors), "r-+", linewidth=2, label="Train")
+  plt.plot(np.sqrt(val_errors), "b-", linewidth=3, label="Test")
+
+
+lin_reg = LinearRegression()
+plot_learning_curves(lin_reg, X, y)
+
+polynomial_regression = Pipeline([
+  ("poly_features", PolynomialFeatures(degree=10, include_bias=False)),
+  ("lin_reg", LinearRegression())
+])
+
+plot_learning_curves(polynomial_regression, X ,y)
